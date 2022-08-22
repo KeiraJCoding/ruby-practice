1c03daec6d7c233c8cf0be74e8403b022bfadc38
Keira Jarvis <keira.jarvis@hotmail.co.uk>
2022-08-22 12:19:38 +0100

    updates

---
+++ file.rb
@@ -0,0 +1,8 @@
+name = "Keira Lucy Jarvis"
+
+if name.include?("e")
+    "the name includes e"
+elsif name.include?("z")
+    "name includes Z"
+else 
+    "the name doesn't include e or z"
---
+++ hello_world.rb
@@ -0,0 +1,8 @@
+puts "Hello World"
+
+word = "Ruby"
+uppercase_word = word.upcase
+puts "#{word} upcased is #{uppercase_word}"
+
+greeting = puts "Hello"
+
